7158be6e0b9e8ce90c0c9ce022ef5d9e696a42b9
Luke Logan <llogan382@gmail.com>
2019-02-19 16:03:04 -0500

    adds event listener, console.log for type of button

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
 
 const display = document.querySelector('.calculator__display');
+
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button.key--button')) {
+      console.log('number');
+    } else if (e.target.matches('button.key--operator')){
+        console.log('operator');
+    } else {
+        console.log('something else');
+    }
+});
