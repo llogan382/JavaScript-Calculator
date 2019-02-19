9f57c278592582304fae89c8e466c9ea2e768fbf
Luke Logan <llogan382@gmail.com>
2019-02-19 15:29:48 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.calculator__keys');
+
+const display = document.querySelector('.calculator__display');
